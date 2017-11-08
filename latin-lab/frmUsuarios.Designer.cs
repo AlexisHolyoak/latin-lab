b3909e83dc8ec1ae28d85a71072c0f994fdada67
alexisholyoak <peraltaholyoak@gmail.com>
2017-11-08 12:58:42 -0500

    formulario para usuarios

---
+++ latin-lab/frmUsuarios.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace latin_lab
+{
+    partial class frmUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.TXTDOCID = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.BTNSALIR = new System.Windows.Forms.Button();
+            this.BTNCANCELAR = new System.Windows.Forms.Button();
+            this.BTNACTUALIZAR = new System.Windows.Forms.Button();
+            this.BTNGUARDAR = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(115, 257);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(715, 72);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(112, 237);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(158, 17);
+            this.label23.TabIndex = 4;
+            this.label23.Text = "ACCESOS ASIGNADOS";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(495, 192);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(103, 17);
+            this.label22.TabIndex = 3;
+            this.label22.Text = "CONTRASEÑA";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(628, 189);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(193, 22);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(103, 194);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(70, 17);
+            this.label21.TabIndex = 1;
+            this.label21.Text = "USUARIO";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 189);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(193, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(96, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "BUSCAR TRABAJADOR";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::latin_lab.Properties.Resources.Zoom_icon1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(299, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 25);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "NOMBRES";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(624, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(49, 95);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(124, 17);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "DOC. ID. TRABAJ.";
+            // 
+            // TXTDOCID
+            // 
+            this.TXTDOCID.Location = new System.Drawing.Point(197, 95);
+            this.TXTDOCID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TXTDOCID.Name = "TXTDOCID";
+            this.TXTDOCID.ReadOnly = true;
+            this.TXTDOCID.Size = new System.Drawing.Size(401, 22);
+            this.TXTDOCID.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(604, 95);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(217, 22);
+            this.textBox5.TabIndex = 15;
+            // 
+            // BTNSALIR
+            // 
+            this.BTNSALIR.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNSALIR.Image = global::latin_lab.Properties.Resources.ic_exit_to_app_black_24dp_1x;
+            this.BTNSALIR.Location = new System.Drawing.Point(703, 346);
+            this.BTNSALIR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTNSALIR.Name = "BTNSALIR";
+            this.BTNSALIR.Size = new System.Drawing.Size(128, 50);
+            this.BTNSALIR.TabIndex = 34;
+            this.BTNSALIR.Text = "SALIR";
+            this.BTNSALIR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTNSALIR.UseVisualStyleBackColor = true;
+            // 
+            // BTNCANCELAR
+            // 
+            this.BTNCANCELAR.Enabled = false;
+            this.BTNCANCELAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNCANCELAR.Image = global::latin_lab.Properties.Resources.Close_2_icon;
+            this.BTNCANCELAR.Location = new System.Drawing.Point(549, 346);
+            this.BTNCANCELAR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTNCANCELAR.Name = "BTNCANCELAR";
+            this.BTNCANCELAR.Size = new System.Drawing.Size(149, 50);
+            this.BTNCANCELAR.TabIndex = 33;
+            this.BTNCANCELAR.Text = "CANCELAR";
+            this.BTNCANCELAR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTNCANCELAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNACTUALIZAR
+            // 
+            this.BTNACTUALIZAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNACTUALIZAR.Image = global::latin_lab.Properties.Resources.Actions_document_edit_icon;
+            this.BTNACTUALIZAR.Location = new System.Drawing.Point(377, 346);
+            this.BTNACTUALIZAR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTNACTUALIZAR.Name = "BTNACTUALIZAR";
+            this.BTNACTUALIZAR.Size = new System.Drawing.Size(169, 50);
+            this.BTNACTUALIZAR.TabIndex = 31;
+            this.BTNACTUALIZAR.Text = "ACTUALIZAR";
+            this.BTNACTUALIZAR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTNACTUALIZAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNGUARDAR
+            // 
+            this.BTNGUARDAR.Enabled = false;
+            this.BTNGUARDAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNGUARDAR.Image = global::latin_lab.Properties.Resources.Save_icon1;
+            this.BTNGUARDAR.Location = new System.Drawing.Point(213, 346);
+            this.BTNGUARDAR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BTNGUARDAR.Name = "BTNGUARDAR";
+            this.BTNGUARDAR.Size = new System.Drawing.Size(159, 50);
+            this.BTNGUARDAR.TabIndex = 30;
+            this.BTNGUARDAR.Text = "GUARDAR";
+            this.BTNGUARDAR.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.BTNGUARDAR.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(197, 139);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(401, 22);
+            this.textBox6.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 17);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "TIPO TRABAJADOR";
+            // 
+            // frmUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 414);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BTNSALIR);
+            this.Controls.Add(this.BTNCANCELAR);
+            this.Controls.Add(this.BTNACTUALIZAR);
+            this.Controls.Add(this.BTNGUARDAR);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.TXTDOCID);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmUsuarios";
+            this.Text = "USUARIOS Y ROLES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox TXTDOCID;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button BTNSALIR;
+        private System.Windows.Forms.Button BTNCANCELAR;
+        private System.Windows.Forms.Button BTNACTUALIZAR;
+        private System.Windows.Forms.Button BTNGUARDAR;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label3;
+    }
+}
